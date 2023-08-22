3a6bdc7ae294ee8247e171e743db99757743caa2
Taeung <park0579@algonquinlive.com>
2023-08-22 16:43:56 -0400

    modify js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ let navlinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -100;
+        let offset = sec.offsetTop -80;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
